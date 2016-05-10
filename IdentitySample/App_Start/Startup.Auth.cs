bc784a4933c6155bbd2e146eebc59f83dc5e61b6
Marc Williams <oti_instructor@hotmail.com>
2016-05-10 10:35:52 -0300

    Resend Confirmation Email

--- IdentitySample/App_Start/Startup.Auth.cs
+++ IdentitySample/App_Start/Startup.Auth.cs
@@ -58,11 +58,11 @@ namespace IdentitySample
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "",
+                ClientSecret = ""
+            });
         }
     }
 }
--- IdentitySample/Controllers/AccountController.cs
+++ IdentitySample/Controllers/AccountController.cs
@@ -106,6 +106,49 @@ namespace IdentitySample.Controllers
             }
         }
 
+
+        //mwilliams
+        //GET:  /Account/ResendEmail
+        [AllowAnonymous]
+        public ActionResult ResendEmail()
+        {
+            return View();
+        }
+
+        //mwilliams
+        //POST:  /Account/ResendEmail
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ResendEmail(ForgotPasswordViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                //validation has passed - resend the email
+                var user = await UserManager.FindByNameAsync(model.Email);
+                if (user == null)
+                {
+                    //Do not reveal that the user does not exist
+                    return View("DisplayEmail");
+                }
+
+                //user has been found 
+                string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                return View("DisplayEmail");
+            }
+
+            //If we reach this point - validation has failed - redisplay form
+            return View(model);
+            
+        }
+
+
+
         //
         // GET: /Account/VerifyCode
         [AllowAnonymous]
@@ -239,10 +282,10 @@ namespace IdentitySample.Controllers
 
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                 // Send an email with this link
-                // string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
-                // var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);		
-                // await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                // return RedirectToAction("ForgotPasswordConfirmation", "Account");
+                string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
+                var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                return RedirectToAction("ForgotPasswordConfirmation", "Account");
             }
 
             // If we got this far, something failed, redisplay form
